1313e01a313477299aca07cace4bb09e65356041
Dmitry Konishchev <konishchev@gmail.com>
2015-04-09 23:56:18 +0300

    server module enhancements

--- server.go
+++ server.go
@@ -3,30 +3,56 @@ package rsspipes
 import (
     "fmt"
     "net/http"
+
     "github.com/KonishchevDmitry/go-rss"
+
+    "rsspipes/util"
 )
 
+var rootRegistered = false
+var log = util.MustGetLogger("server")
+
+func Serve(addressPort string) error {
+    log.Info("Listening on %s...", addressPort)
+
+    if !rootRegistered {
+        register("/", http.NotFound)
+    }
+
+    return http.ListenAndServe(addressPort, nil)
+}
+
 func Register(path string, generator func() (*rss.Feed, error)) {
-    http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+    register(path, func(w http.ResponseWriter, r *http.Request) {
         generate(w, r, generator)
     })
 }
 
-func Serve() {
-    http.ListenAndServe(":8080", nil)
+func register(path string, handler func(http.ResponseWriter, *http.Request)) {
+    http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+        log.Info("%s %s", r.Method, r.RequestURI)
+        handler(w, r)
+    })
+
+    if path == "/" {
+        rootRegistered = true
+    }
 }
 
 func generate(w http.ResponseWriter, r *http.Request, generator func() (*rss.Feed, error)) {
+    var rssData []byte
+
     feed, err := generator()
+    if err == nil {
+        rssData, err = rss.Generate(feed)
+    }
+
     if err != nil {
         error := fmt.Sprintf("Failed to generate the RSS feed: %s.", err)
         http.Error(w, error, http.StatusInternalServerError)
         return
     }
 
-    // FIXME
-    rss, err := rss.Generate(feed)
-
     w.Header().Set("Content-Type", "application/rss+xml")
-    w.Write([]byte(rss))
+    w.Write(rssData)
 }
