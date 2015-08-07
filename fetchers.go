853eb184e89ce7be73c5d455782aa5e78a915e0b
Dmitry Konishchev <konishchev@gmail.com>
2015-08-07 21:56:12 +0300

    Add HTML fetcher

--- fetchers.go
+++ fetchers.go
@@ -7,6 +7,12 @@ import (
     "mime"
     "net/http"
     "os"
+    "strings"
+
+    "golang.org/x/net/html"
+
+    "code.google.com/p/go-charset/charset"
+    _ "code.google.com/p/go-charset/data"
 
     "github.com/KonishchevDmitry/go-rss"
 )
@@ -19,17 +25,19 @@ type FutureFeed chan FutureFeedResult
 type FetchFunc func(string) (*rss.Feed, error)
 
 func FetchUrl(url string) (feed *rss.Feed, err error) {
+    defer func() { err = handleError(url, err) }()
+
     feed, err = rss.Get(url)
     if err != nil {
-        logError(url, err)
         return
     }
+
     sortItems(feed)
     return
 }
 
 func FetchFile(path string) (feed *rss.Feed, err error) {
-    defer func() { logError(path, err) }()
+    defer func() { err = handleError(path, err) }()
 
     file, err := os.Open(path)
     if err != nil {
@@ -73,6 +81,8 @@ func GetFutures(futureFeeds ...FutureFeed) (feeds []*rss.Feed, err error) {
 }
 
 func FetchData(url string, allowedMediaTypes []string) (mediaType string, data string, err error) {
+    defer func() { err = handleError(url, err) }()
+
     client := &http.Client{
         Timeout: rss.DefaultGetParams.Timeout,
     }
@@ -103,6 +113,41 @@ func FetchData(url string, allowedMediaTypes []string) (mediaType string, data s
     return
 }
 
+func FetchHtml(url string) (data string, err error) {
+    defer func() { err = handleError(url, err) }()
+
+    _, data, err = FetchData(url, []string{"text/html"})
+    if err != nil {
+        return
+    }
+
+    doc, err := html.Parse(strings.NewReader(data))
+    if err != nil {
+        return
+    }
+
+    encoding := getHtmlCharset(doc, url)
+    if encoding == "" {
+        return
+    }
+
+    charsetReader, err := charset.NewReader(encoding, strings.NewReader(data))
+    if err != nil {
+        err = fmt.Errorf("The document has an unknown charset encoding: %s.", encoding)
+        return
+    }
+
+    decodedBytes, err := ioutil.ReadAll(charsetReader)
+    if err != nil {
+        err = fmt.Errorf("Failed to decode the document using %s charset: %s", err)
+        return
+    }
+
+    data = string(decodedBytes)
+
+    return
+}
+
 func checkMediaType(response *http.Response, allowedMediaTypes []string) (mediaType string, err error) {
     contentType := response.Header.Get("Content-Type")
     mediaType, _, err = mime.ParseMediaType(contentType)
@@ -129,8 +174,62 @@ func checkMediaType(response *http.Response, allowedMediaTypes []string) (mediaT
     return
 }
 
-func logError(uri string, err error) {
-    if err != nil {
-        log.Error("Failed to fetch %s: %s", uri, err)
+func getHtmlCharset(doc *html.Node, uri string) (charset string) {
+    node := findHtmlNode(doc, "html")
+    if node != nil {
+        node = findHtmlNode(node, "head")
     }
+    if node == nil {
+        return
+    }
+
+    isHttpCharset := true
+
+    for node := node.FirstChild; node != nil; node = node.NextSibling {
+        if node.Type != html.ElementNode || node.Data != "meta" {
+            continue
+        }
+
+        attrs := make(map[string]string)
+        for _, attr := range node.Attr {
+            attrs[strings.ToLower(attr.Key)] = strings.ToLower(attr.Val)
+        }
+
+        if attrs["http-equiv"] == "content-type" {
+            _, params, err := mime.ParseMediaType(attrs["content"])
+
+            if err != nil {
+                log.Error("Got an invalid content type of '%s' from <meta http-equiv=\"Content-Type\"> tag: '%s'.",
+                          uri, attrs["content"])
+            } else if params["charset"] != "" && isHttpCharset {
+                charset = params["charset"]
+            }
+        }
+
+        if attrs["charset"] != "" {
+            charset = attrs["charset"]
+            isHttpCharset = false
+        }
+    }
+
+    return
+}
+
+func findHtmlNode(node *html.Node, name string) *html.Node {
+    for node = node.FirstChild; node != nil; node = node.NextSibling {
+        if node.Type == html.ElementNode && node.Data == name {
+            return node
+        }
+    }
+
+    return nil
+}
+
+func handleError(uri string, err error) error {
+    if err != nil {
+        err = fmt.Errorf("Failed to fetch %s: %s", uri, err)
+        log.Error("%s", err)
+    }
+
+    return err
 }
